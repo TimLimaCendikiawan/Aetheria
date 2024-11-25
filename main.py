63842e6dc639b82c8037aac4da057e62b2cfb90a
Habib <habib.erdian@gmail.com>
2024-11-25 14:56:03 +0700

    added get_instance_id

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import fitz  # PyMuPDF
 import os
 from dotenv import load_dotenv
 import re
+import requests
 
 load_dotenv()
 
@@ -107,6 +108,27 @@ class ConversationManager:
     def update_system_message(self, new_message):
         self.system_message = new_message
         self.reset_conversation_history()
+
+def get_instance_id():
+    """Retrieve the EC2 instance ID from AWS metadata using IMDSv2."""
+    try:
+        # Step 1: Get the token
+        token = requests.put(
+            "http://169.254.169.254/latest/api/token",
+            headers={"X-aws-ec2-metadata-token-ttl-seconds": "21600"},
+            timeout=1
+        ).text
+
+        # Step 2: Use the token to get the instance ID
+        instance_id = requests.get(
+            "http://169.254.169.254/latest/meta-data/instance-id",
+            headers={"X-aws-ec2-metadata-token": token},
+            timeout=1
+        ).text
+        return instance_id
+    except requests.exceptions.RequestException:
+        return "Instance ID not available (running locally or error in retrieval)"
+
 
 ### Streamlit code ###
 st.title("AI Chatbot")
